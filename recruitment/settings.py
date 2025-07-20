7908a7bcccddcfcf95f00d8bc85fd8c6ecf8ba47
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2025-07-20 11:47:51 +0300

    add postgresql settings and install new library for postgresql

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hi(3h_v)q2fegq(l6&uzzq@m6^)u0gh3al%p6m)dhuj=p7b&lh"
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-hi(3h_v)q2fegq(l6&uzzq@m6^)u0gh3al%p6m)dhuj=p7b&lh")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
 
 
 # Application definition
@@ -87,8 +88,12 @@ WSGI_APPLICATION = "recruitment.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
+        "NAME": config("DB_NAME", default="ik_recruitment"),
+        "USER": config("DB_USER", default="ik_user"),
+        "PASSWORD": config("DB_PASSWORD", default="ik_password"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
