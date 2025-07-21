54a913a54921961c8350a889f1c9c9efbe59ff99
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2025-07-21 13:30:52 +0300

    remove todo

--- recruitment/celery_schedules.py
+++ recruitment/celery_schedules.py
@@ -1,6 +1,6 @@
 CELERYBEAT_SCHEDULE = {
     "deactivate-expired-job-postings": {
         "task": "apps.job_posting.tasks.DeactivateExpiredJobPostings",
-        "schedule": 180,  # todo: fix 3min
+        "schedule": 180,
     },
 }
