95f2ddd24604f7b7d84ee5b19f55f7b710d7a23e
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2025-07-24 12:16:01 +0300

    update JobPostingAdmin page for authorization

--- apps/job_posting/admin.py
+++ apps/job_posting/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from apps.hr_user.models import HRUser
+from apps.hr_company.models import HRCompany
+from apps.client_company.models import ClientCompany
 from .models import JobPosting
 
 
@@ -17,3 +20,12 @@ class JobPostingAdmin(admin.ModelAdmin):
         "closing_date",
         "is_active",
     ]
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "hr_user":
+            kwargs["queryset"] = HRUser.objects.filter(id=request.user.id)
+        if db_field.name == "hr_company":
+            kwargs["queryset"] = HRCompany.objects.filter(hr_users=request.user)
+        if db_field.name == "client_company":
+            kwargs["queryset"] = ClientCompany.objects.filter(authorized_hr_users=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
